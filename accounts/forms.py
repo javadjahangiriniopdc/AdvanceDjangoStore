041fc54e98174c737e5d5d87175190dbaac6b7e8
aylin soufizadeh <aylinsoufizadeh2000@gmail.com>
2023-09-24 18:28:48 +0330

    fix save method and makemigratin and create database

--- accounts/forms.py
+++ accounts/forms.py
@@ -20,7 +20,7 @@ class UserCreationForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.changed_data['password1'])
+        user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
